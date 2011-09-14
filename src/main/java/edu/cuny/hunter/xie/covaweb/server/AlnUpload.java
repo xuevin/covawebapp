77ab8ad50bd36bfd156deb8a519f3c82604a6776
Vincent <xue.vin@gmail.com>
2011-09-14 14:31:22 -0400

    Added Upload Widget via UIBinding

---
+++ src/main/java/edu/cuny/hunter/xie/covaweb/server/AlnUpload.java
@@ -0,0 +1,12 @@
+package edu.cuny.hunter.xie.covaweb.server;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+
+import gwtupload.server.UploadAction;
+
+public class AlnUpload extends UploadAction{
+  
+  public AlnUpload(){}
+
+}
