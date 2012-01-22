8d97ff27c0a1ae951025af209391a825e11c4f96
Vincent <xue.vin@gmail.com>
2012-01-22 13:20:18 -0500

    Removed hard coded file locations.

--- src/main/java/edu/cuny/hunter/xie/covaweb/server/local/RunHMMER.java
+++ src/main/java/edu/cuny/hunter/xie/covaweb/server/local/RunHMMER.java
@@ -5,29 +5,35 @@ import java.io.File;
 import org.biojava3.core.sequence.MultipleSequenceAlignment;
 import org.biojava3.core.sequence.ProteinSequence;
 import org.biojava3.core.sequence.compound.AminoAcidCompound;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import edu.cuny.hunter.xie.covaweb.server.parsers.StockholmParser;
 
 public class RunHMMER {
-  
-  private static String HMMERDIR = "/home/chrnovx/Applications/hmmer-3.0-linux-intel-x86_64/binaries";
+  // TODO Move these options into a config file
+  private String hmmerDir;
   private MultipleSequenceAlignment<ProteinSequence,AminoAcidCompound> alignment;
+  Logger logger = LoggerFactory.getLogger(RunHMMER.class);
   
-  public RunHMMER() {
-
+  public RunHMMER(String hmmerDir) {
+    this.hmmerDir=hmmerDir;
   }
-  
-  public void run() {
-    try {
-      // TODO Move these options into a config file
-      File hmm = new File(HMMERDIR + "/foo.hmm");
-      String unalignedSeqDir = " /home/chrnovx/Dropbox/workspace/covawebapp/src/test/resources/combined_w_seed.unaligned";
-      
-      Process p = Runtime.getRuntime().exec(
-          HMMERDIR + "/hmmalign " + hmm.getAbsolutePath() + unalignedSeqDir);
-      
-      alignment = StockholmParser.getMSA(p.getInputStream());
 
+  public void run(String... args) {
+    try {
+      
+      //Convert array to input
+      //TODO find a better way to do this - This is ugly and not robust
+      String opts=" ";
+      for(String s:args){
+        opts+=" " +s; 
+      }
+      
+      
+      Process p = Runtime.getRuntime().exec(hmmerDir + "/hmmalign" + opts);
+      alignment = StockholmParser.getMSA(p.getInputStream());
+      logger.debug("The alignment is as follows:\n{}",alignment.toString());
     } catch (Exception err) {
       err.printStackTrace();
     }
