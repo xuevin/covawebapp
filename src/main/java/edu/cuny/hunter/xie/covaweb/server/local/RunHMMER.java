00c462cbf1b1372bdd638da7920fb5647f8ba124
Vincent <xue.vin@gmail.com>
2012-01-22 20:49:55 -0500

    Removed hard coded file paths

--- src/main/java/edu/cuny/hunter/xie/covaweb/server/local/RunHMMER.java
+++ src/main/java/edu/cuny/hunter/xie/covaweb/server/local/RunHMMER.java
@@ -1,7 +1,5 @@
 package edu.cuny.hunter.xie.covaweb.server.local;
 
-import java.io.File;
-
 import org.biojava3.core.sequence.MultipleSequenceAlignment;
 import org.biojava3.core.sequence.ProteinSequence;
 import org.biojava3.core.sequence.compound.AminoAcidCompound;
@@ -11,7 +9,6 @@ import org.slf4j.LoggerFactory;
 import edu.cuny.hunter.xie.covaweb.server.parsers.StockholmParser;
 
 public class RunHMMER {
-  // TODO Move these options into a config file
   private String hmmerDir;
   private MultipleSequenceAlignment<ProteinSequence,AminoAcidCompound> alignment;
   Logger logger = LoggerFactory.getLogger(RunHMMER.class);
@@ -20,19 +17,13 @@ public class RunHMMER {
     this.hmmerDir=hmmerDir;
   }
 
-  public void run(String... args) {
+  public void run(String hmmPath, String unalignedSequences) {
     try {
       
-      //Convert array to input
-      //TODO find a better way to do this - This is ugly and not robust
-      String opts=" ";
-      for(String s:args){
-        opts+=" " +s; 
-      }
+      Process p = Runtime.getRuntime().exec(hmmerDir + "/hmmalign " + hmmPath + " " + unalignedSequences);
       
-      
-      Process p = Runtime.getRuntime().exec(hmmerDir + "/hmmalign" + opts);
       alignment = StockholmParser.getMSA(p.getInputStream());
+      
       logger.debug("The alignment is as follows:\n{}",alignment.toString());
     } catch (Exception err) {
       err.printStackTrace();
